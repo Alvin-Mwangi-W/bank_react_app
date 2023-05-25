576d2550d02470ef3123bd32d6aeb6f1a455ed91
Alvin-Mwangi-W <alvin.wacira.m@gmail.com>
2023-05-25 12:18:10 +0300

    tailwind fix

---
+++ bank_app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./style";
+
+const App = () => (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className="`${styles.paddingX} ${styles.flexCenter}`">
+        <h1>34</h1>
+      </div>
+    </div>
+);
+
+export default App;
