9dbb75e08901c526f374251e99b408c4e2b01016
Alvin-Mwangi-W <alvin.wacira.m@gmail.com>
2023-05-30 22:09:49 +0300

    Navbar & Hero

--- bank_app/src/App.jsx
+++ bank_app/src/App.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import styles from "./style";
+import { Navbar, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer, Hero } from "./components";
 
 const App = () => (
     <div className="bg-primary w-full overflow-hidden">
-      <div className="`${styles.paddingX} ${styles.flexCenter}`">
-        <h1>34</h1>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business /> 
+          <Billing /> 
+          <CardDeal /> 
+          <Testimonials /> 
+          <Clients /> 
+          <CTA />
+          <Footer /> 
+        </div>
       </div>
     </div>
 );
