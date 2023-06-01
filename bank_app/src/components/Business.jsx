786255d2f7d457653841bb85642c90bd02650009
Alvin-Mwangi-W <alvin.wacira.m@gmail.com>
2023-06-01 12:09:11 +0300

    hero sections and stats

--- bank_app/src/components/Business.jsx
+++ bank_app/src/components/Business.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import { features } from "../constants"
+import styles, { layout } from "../style"
+import Button from "./Button"
 
 const Business = () => {
   return (
-    <div>Business</div>
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2>You do the business, <br /> we’ll handle the money.</h2>
+      </div>
+    </section>
   )
 }
 
--- bank_app/src/components/GetStarted.jsx
+++ bank_app/src/components/GetStarted.jsx
@@ -4,7 +4,7 @@ import { arrowUp } from '../assets'
 
 const GetStarted = () => {
   return (
-    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+    <div className={`${styles.flexCenter} w-[130px] h-[130px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
       <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full` }>
         <div className={`${styles.flexStart} flex-row`}>
           <p className='font-poppins font-medium text-[18px] leading-[28px] mr-2'>
