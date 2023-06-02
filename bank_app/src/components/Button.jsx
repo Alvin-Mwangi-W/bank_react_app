084b4cb3cab415f4cb7e9fea7800979323e85ea7
Alvin-Mwangi-W <alvin.wacira.m@gmail.com>
2023-06-02 13:21:11 +0300

    Billing section

--- bank_app/src/components/Button.jsx
+++ bank_app/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ styles }) => {
   return (
-    <div type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-md ${styles}`}>
+    <div type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-md ${styles} cursor-pointer`}>
       Get Started
     </div>
   )
